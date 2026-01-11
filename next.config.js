a50a04fed430e395dda319dcbea50db0339fc333
cloudycabin1 <cloudycabin1@gmail.com>
2026-01-11 09:54:44 +0200

    Update and rename dummy.txt to next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `fs` module
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
